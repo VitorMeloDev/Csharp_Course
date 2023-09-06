117927a789ee7a775208d034efe4679325578847
Vitor Melo <89869031+VitorMeloDev@users.noreply.github.com>
2023-09-06 15:00:50 -0300

    Exercicio proposto set

---
+++ Exercício proposto (Set)/Exercício proposto (Set)/Entities/Student.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercício_proposto__Set_.Entities
+{
+    class Student
+    {
+        public int Id { get; set; }
+
+        public Student(int id)
+        { this.Id = id; }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(obj == null || !(obj is Student))
+            {  return false; }
+            
+            Student student = obj as Student;
+            
+            return Id.Equals(student.Id);
+        }
+    }
+}
---
+++ Exercício proposto (Set)/Exercício proposto (Set)/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using Exercício_proposto__Set_.Entities;
+
+
+namespace Exercício_proposto__Set_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Student> students = new HashSet<Student>();
+
+            try
+            {
+                for (int i = 1; i <= 3; i++)
+                {
+                    string course = "A";
+
+                    switch (i)
+                    {
+                        case 1:
+                            course = "A? ";
+                            break;
+                        case 2:
+                            course = "B? ";
+                            break;
+                        case 3:
+                            course = "C? ";
+                            break;
+                    }
+
+                    Console.Write("How many students for course " + course);
+                    int many = int.Parse(Console.ReadLine());
+
+                    for (int n = 1; n <= many; n++)
+                    {
+                        int val = int.Parse(Console.ReadLine());
+                        students.Add(new Student(val));
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("Total students: " + students.Count);
+            } 
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        
+    }
+}
