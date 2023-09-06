ea46db71f58cb8998a87200221a2a398f8d857ca
Vitor Melo <89869031+VitorMeloDev@users.noreply.github.com>
2023-09-06 15:44:07 -0300

    Exercicio de Dictionary

---
+++ ExercicioPropostoDictionary/ExercicioPropostoDictionary/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter file full path: ");
+        string path = Console.ReadLine();
+        Dictionary<string, int> dic = new Dictionary<string, int>();
+
+        try
+        {
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(',');
+                    string name = line[0];
+                    int value = int.Parse(line[1]);
+                    if (dic.ContainsKey(name))
+                    {
+                        value += dic[name];
+                        dic[name] = value;
+                    }
+                    else
+                    {
+                        dic[name] = value;
+                    }
+                }
+            }
+
+            Console.WriteLine("Total: ");
+            foreach (var it in dic)
+            {
+                Console.WriteLine(it.Key + ": " + it.Value);
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+    }
+}
